52d425873543b1ddb31ca9862ffca3c477eccc8f
pyCity <dwf1421@jagmail.southalabama.edu>
2019-04-27 15:52:30 -0500

    Minor changes

--- setup.sh
+++ setup.sh
@@ -11,14 +11,14 @@ while ! [ -e "requirements.txt" ] && [ -x "$(command -v pip3)" && "$(command -v 
 
     # Check for python3 and pip3
     if ! [ -x "$(command -v pip3)" || "$(command -v python3)" ]; then
-        echo "Either ip3 or python3 not detected. installing pip3"
+        echo "Either pip3 or python3 not detected. installing pip3"
         apt-get update -y && apt-get -y install python3 python3-pip
     fi
     # Check for requirements.txt
 
     if ! [ -e "requirements.txt" ]; then
         echo "File not found. Creating requirements.txt"
-        echo -e "pycryptodome==3.7.3\n" > requirements.txt
+        echo -e "pycryptodome==3.7.3\ntqdm==4.31.1\n" > requirements.txt
     fi
 done
 
